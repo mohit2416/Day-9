1500d8d678fbed91cce41212f78ed1fa7c4e8a13
mohit2416 <mohitdiwakar2412@gmail.com>
2026-02-10 12:20:38 +0530

    first commit

---
+++ Backend/src/app.js
@@ -0,0 +1,57 @@
+const express = require("express")
+const noteModel = require("./models/note.model")
+const cors = require("cors")
+
+const app = express()
+
+app.use(express.json())
+app.use(cors())
+
+app.post("/api/notes", async (req,res) => {
+    const { title, description } = req.body
+
+   const note = await noteModel.create({
+        title, description 
+    })
+
+    res.status(201).json({
+        message: "Note Created Successfully",
+        note
+    })
+})
+
+app.get("/api/notes", async (req,res) => {
+  const notes = await noteModel.find()
+
+  res.status(200).json({
+    message: "Note fetched successfully",
+    notes
+  })
+})
+
+app.delete("/api/notes/:id", async (req,res) => {
+    const id = req.params.id
+
+   await noteModel.findByIdAndDelete(id)
+
+    res.status(200).json({
+        message: "Note Delete successsfully."
+    })
+    
+})
+
+app.patch("/api/notes/:id", async (req,res) => {
+
+    const id = req.params.id
+    const { description } = req.body
+
+    await noteModel.findByIdAndUpdate(id, { description })
+
+    res.status(200).json({
+        message: "Note updated Successfully",
+
+    })
+})
+
+
+module.exports = app
