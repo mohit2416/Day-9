9270e9e4a4a8244a0af4d57a37cfe15a5e69436e
mohit2416 <mohitdiwakar2412@gmail.com>
2026-02-10 16:48:07 +0530

    update

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -1,11 +1,13 @@
 const express = require("express")
 const noteModel = require("./models/note.model")
 const cors = require("cors")
+const path = require("path")
 
 const app = express()
 
 app.use(express.json())
 app.use(cors())
+app.use(express.static("./public"))
 
 app.post("/api/notes", async (req,res) => {
     const { title, description } = req.body
@@ -53,5 +55,8 @@ app.patch("/api/notes/:id", async (req,res) => {
     })
 })
 
+app.use('*name',(req,res) => {
+    res.sendFile(path.join(__dirname,"..","/public/index.html"))
+})
 
 module.exports = app
--- Frontend/Frontend/src/App.jsx
+++ Frontend/Frontend/src/App.jsx
@@ -1,43 +1,66 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import axios from "axios"
 
 const App = () => {
 
-  const [notes, setNotes] = useState([
-    {
-      title: "test title 1",
-      description: "test description 1",
-    },
-    {
-      title: "test title 2",
-      description: "test description 2",
-    },
-    {
-      title: "test title 3",
-      description: "test description 3",
-    },
-    {
-      title: "test title 4",
-      description: "test description 4",
-    },
-  ])
+  const [notes, setNotes] = useState([])
 
-  axios.get('http://localhost:3000/api/notes')
-  .then((res) => {
-    setNotes(res.data.notes)
-  })
 
+  function fetchNote() {
+    axios.get('http://localhost:3000/api/notes')
+      .then((res) => {
+        setNotes(res.data.notes)
+      })
+  }
+
+
+  useEffect(() => {
+    fetchNote()
+  }, [])
+
+    function handelSubmit(e){
+      e.preventDefault()
+      const { title, description } = e.target.elements
+
+      console.log(title.value,description.value);
+
+      axios.post("http://localhost:3000/api/notes",{
+        title:title.value,
+        description:description.value
+      })
+      .then(res => {
+        console.log(res.data);
+
+         fetchNote()
+      })
+      
+    }
+
+    function handelDeleteNote(noteID){
+      axios.delete("http://localhost:3000/api/notes/"+noteID)
+      .then(res => {
+        console.log(res.data)
+        fetchNote()
+      })
+    }
   return (
     <>
+
+    <form className='note-create-form' onSubmit={handelSubmit}>
+    <input name='title' type="text" placeholder='Enter Title' />
+    <input name='description' type="text" placeholder='Enter Description '/>
+    <button>Create Note</button>
+    </form>
+
       <div className="notes">
         {notes.map(note => {
           return <div className="note">
             <h1>{note.title}</h1>
             <p>{note.description}</p>
+            <button onClick={()=>{handelDeleteNote(note._id)}}>Delete Note</button>
           </div>
         })
         }
-
       </div>
     </>
   )
